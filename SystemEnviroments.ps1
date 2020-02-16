9ff5992feeec46d0668c623071ae625a42299d6e
Vinicius Alberkovics <viniciusa.d@hotmail.com>
2020-02-15 23:21:09 -0300

    Script for set Enviroment Variables - Windows

---
+++ SystemEnviroments.ps1
@@ -0,0 +1,29 @@
+# System Environment Variable
+# [Environment]::SetEnvironmentVariable("MyTestVariable", "MyTestValue", "Machine")
+
+# User Environment Variable
+# [Environment]::SetEnvironmentVariable("MyTestVariable", "MyTestValue", "User")
+
+
+# Run Scripts with Admin
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+    exit 
+}
+
+# Script
+$targetVariable = "Machine";
+
+$listVariables = @{
+    "V1"="valor1"; 
+    "V2"="valor2"
+};
+
+foreach($lv in $listVariables.Keys) {
+   # Write-Host "${lv} : $($listVariables.Item($lv))";
+   $item = [Environment]::GetEnvironmentVariable($lv, $targetVariable);
+
+   if ([String]::IsNullOrWhiteSpace($item)){
+       [Environment]::SetEnvironmentVariable($lv, $($listVariables.Item($lv)), $targetVariable);
+   }
+}
